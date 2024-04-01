064fefb74d34efe3358a246e0cafb2423dfed605
maynestream-dev <chris@maynestream.dev>
2024-04-01 15:17:46 +0100

    Improve bootstrap logging and flow

--- macos/install-xcode.sh
+++ macos/install-xcode.sh
@@ -13,12 +13,11 @@ if [ $? -ne 0 ]; then
   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
   PROD=$(softwareupdate -l |
     grep "\*.*Command Line" |
-    head -n 1 | awk -F"*" '{print $2}' |
+    head -n 1 | awk -F": " '{print $2}' |
     sed -e 's/^ *//' |
     tr -d '\n')
-  softwareupdate -i "$PROD" -v;
+  softwareupdate -i "$PROD" --verbose;
 else
   echo "Xcode CLI tools OK"
 fi
 
-softwareupdate –install-rosetta
