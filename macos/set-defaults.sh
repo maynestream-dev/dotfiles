cf68964e5c0b5a6a1392a64064eb1c86635810b4
maynestream-dev <chris@maynestream.dev>
2024-04-01 12:50:47 +0100

    Initial attempt at capturing mac zsh setup

---
+++ macos/set-defaults.sh
@@ -0,0 +1,15 @@
+# Sets reasonable macOS defaults.
+#
+# Or, in other words, set shit how I like in macOS.
+#
+# The original idea (and a couple settings) were grabbed from:
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+#
+# Run ./set-defaults.sh and you'll be good to go.
+
+# Show the ~/Library folder.
+chflags nohidden ~/Library
+
+# Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
