07e988851d20c729de138a4e934a467bee8d1a8a
maynestream-dev <chris@maynestream.dev>
2024-04-14 20:36:03 +0100

    Add path/fpath and topics

---
+++ macos/alias.zsh
@@ -0,0 +1,1 @@
+alias mas='reattach-to-user-namespace mas'
--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -13,3 +13,5 @@ chflags nohidden ~/Library
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+echo "Set system defaults"
