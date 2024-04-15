ecd024515a411ac5ec95ab4b8d388e893d3d3db9
maynestream-dev <chris@maynestream.dev>
2024-04-15 01:39:30 +0100

    Add rust/cargo and nvim installs

---
+++ nvim/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+if [[ ! -d "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim ]]; then
+  info "Cloning nvim kickstart"
+  git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+fi
