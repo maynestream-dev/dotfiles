4287f24ab4dc073957458ba6249962965cc36766
Chris Mayne <chris@maynestream.dev>
2024-04-15 02:15:50 +0100

    Only install rust if not already (#3)

--- rust/install.sh
+++ rust/install.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-if command -v rustup > /dev/null; then
-  if [[ ! $(rustup toolchain list | wc -l) -gt 0 ]]; then
-    rustup-init -y -q
-  fi
+function install_rustup() {
+  rustup-init -y -q --no-modify-path
+  source "${XDG_DATA_HOME}/cargo/env"
+}
+
+if ! command -v rustup > /dev/null 2>&1; then
+  install_rustup
+elif [ ! $(rustup toolchain list | wc -l) -gt 0 ]; then
+  install_rustup
 fi
 
 echo "Rustup installed: $(rustup toolchain list)"
