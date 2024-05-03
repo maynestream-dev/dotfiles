1d9956fc1137b26619da1f68320465819d10840f
maynestream-dev <chris@maynestream.dev>
2024-05-04 00:30:39 +0100

    make zsh executable

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -4,4 +4,4 @@ ALACRITTY_HOME="${XDG_CONFIG_HOME:-$HOME/.config}/alacritty"
 mkdir -p "$ALACRITTY_HOME"
 [[ ! -d "$ALACRITTY_HOME/themes" ]] && git clone https://github.com/alacritty/alacritty-theme "$ALACRITTY_HOME/themes"
 
- ln -s "$DOTFILES/alacritty/alacritty.toml" "$ALACRITTY_HOME"
+link-file "$DOTFILES/alacritty/alacritty.toml" "$ALACRITTY_HOME"
